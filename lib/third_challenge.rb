2c007972b0903d5362241e2f8251399ad0cf9c76
Samantha brake <sammy.brake@gmail.com>
2018-09-11 19:25:43 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
         :age => "50"
         },
       :matriarch => {
-       :name => "Lady Capulet"
+       :name => "Lady Capulet",
        :age => "51"
         },
       :heroine => {
